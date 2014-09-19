ef341b45dd2611d520965d13757eda0ba6fb1cf7
Vu Bui <vubui@qasymphony.com>
2014-09-19 17:57:30 +0700

    fix sql init script and startup command

--- mysql/base/entrypoint.sh
+++ mysql/base/entrypoint.sh
@@ -12,13 +12,14 @@ if [ ! -f $MYSQL_INIT_SCRIPT ]; then
   cat > $MYSQL_INIT_SCRIPT \
 <<-EOSQL
 DELETE FROM mysql.user;
+FLUSH PRIVILEGES;
 CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 DROP DATABASE IF EXISTS test;
 EOSQL
 
-  mysql -u root --password=root < $MYSQL_INIT_SCRIPT
+  mysql --user=root --password=root < $MYSQL_INIT_SCRIPT
 fi
 
 if [ -n $1 ] && [ -x $1 ]; then
