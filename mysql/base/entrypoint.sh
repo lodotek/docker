a01f27b0c0be4b28e86d94d1e8b78c0d6f13240c
Vu Bui <vubui@qasymphony.com>
2014-09-21 00:07:05 +0700

    update mysql binding address and startup script

--- mysql/base/entrypoint.sh
+++ mysql/base/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-service mysql start
-
 MYSQL_INIT_SCRIPT='/tmp/mysql-init.sql'
 
 if [ ! -f $MYSQL_INIT_SCRIPT ]; then
@@ -15,16 +13,14 @@ DELETE FROM mysql.user where User <> 'debian-sys-maint';
 CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
 GRANT PROXY ON ''@'' TO 'root'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
 DROP DATABASE IF EXISTS test;
 EOSQL
 
+  sed -i 's/\(bind-address\)/#\1/' /etc/mysql/my.cnf
+  service mysql start
   mysql --password='root' < "$MYSQL_INIT_SCRIPT"
+  service mysql stop
 fi
 
-if [ -n "$1" ] && [ -x "$(which $1)" ]; then
-  exec "$@"
-else
-  exec tail -f /var/log/mysql/error.log
-fi
+exec "$@"
 
