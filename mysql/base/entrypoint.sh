306bb2153ff606c4ff6f984588bd59befd3bd45f
Vu Bui <vubui@qasymphony.com>
2015-04-15 11:21:58 +0700

    update mysql entrypoint

--- mysql/base/entrypoint.sh
+++ mysql/base/entrypoint.sh
@@ -3,27 +3,33 @@
 MYSQL_CONF='/conf'
 MYSQL_DATA='/data'
 MYSQL_LOGS='/logs'
+MYSQL_CONFIGURED="$MYSQL_CONF/configured"
 MYSQL_INIT_SCRIPT="$MYSQL_DATA/mysql-init.sql"
 
-if [ ! "$(ls -A $MYSQL_CONF)" ]; then
-  mv /etc/mysql/* /conf
-  rm -r /etc/mysql
-  ln -s $MYSQL_CONF /etc/mysql
-  if [ ! "$(ls -A $MYSQL_DATA)" ]; then
-    mv /var/lib/mysql/* $MYSQL_DATA/
-    mv /var/log/mysql/* $MYSQL_LOGS/
-  fi
-  rm -r /var/lib/mysql /var/log/mysql
-  ln -s $MYSQL_DATA /var/lib/mysql
-  ln -s $MYSQL_LOGS /var/log/mysql
-  chown -R mysql:mysql $MYSQL_CONF $MYSQL_DATA $MYSQL_LOGS /etc/mysql /var/lib/mysql /var/log/mysql
+link() {
+  if [ ! -L $2 ]; then
+    if [ ! "$(ls -A $1)" ]; then
+      mv $2/* $1/
+    fi
 
+    rm -r $2
+    ln -s $1 $2
+    chown -R mysql:mysql $1 $2
+  fi
+}
+
+link $MYSQL_CONF /etc/mysql
+link $MYSQL_DATA /var/lib/mysql
+link $MYSQL_LOGS /var/log/mysql
+
+if [ ! -f $MYSQL_CONFIGURED ]; then
   MYSQL_CONF_FILE="$MYSQL_CONF/mysql.conf.d/mysqld.cnf"
   if [ ! -f $MYSQL_CONF_FILE ]; then
     MYSQL_CONF_FILE="$MYSQL_CONF/my.cnf"
   fi
   sed -i 's/\(bind-address\)/#\1/' $MYSQL_CONF_FILE
   sed -i '/\[mysqld\]/askip_name_resolve' $MYSQL_CONF_FILE
+  touch $MYSQL_CONFIGURED
 fi
 
 if [ ! -f $MYSQL_INIT_SCRIPT ]; then
@@ -31,7 +37,7 @@ if [ ! -f $MYSQL_INIT_SCRIPT ]; then
     export MYSQL_ROOT_PASSWORD=root
   fi
 
-  cat > "$MYSQL_INIT_SCRIPT" \
+  cat > $MYSQL_INIT_SCRIPT \
 <<-EOSQL
 DELETE FROM mysql.user where User <> 'debian-sys-maint';
 CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
