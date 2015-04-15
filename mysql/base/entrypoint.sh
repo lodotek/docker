f7804136e9f830c6ded9752a5042b61d94d0fad3
Vu Bui <vubui@qasymphony.com>
2015-04-15 09:47:53 +0700

    add config dir to mysql

--- mysql/base/entrypoint.sh
+++ mysql/base/entrypoint.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+MYSQL_CONF='/conf'
 MYSQL_DATA='/data'
 MYSQL_LOGS='/logs'
 MYSQL_INIT_SCRIPT="$MYSQL_DATA/mysql-init.sql"
-MYSQL_CONFIGURED='/tmp/configured'
 
-if [ ! -f $MYSQL_CONFIGURED ]; then
-  MYSQL_CONF_FILE='/etc/mysql/mysql.conf.d/mysqld.cnf'
-  if [ ! -f $MYSQL_CONF_FILE ]; then
-    MYSQL_CONF_FILE='/etc/mysql/my.cnf'
-  fi
-  sed -i 's/\(bind-address\)/#\1/' $MYSQL_CONF_FILE
-  sed -i '/\[mysqld\]/askip_name_resolve' $MYSQL_CONF_FILE
-  touch $MYSQL_CONFIGURED
-
-  if [ ! -f $MYSQL_INIT_SCRIPT ]; then
+if [ ! "$(ls -A $MYSQL_CONF)" ]; then
+  mv /etc/mysql/* /conf
+  rm -r /etc/mysql
+  ln -s $MYSQL_CONF /etc/mysql
+  if [ ! "$(ls -A $MYSQL_DATA)" ]; then
     mv /var/lib/mysql/* $MYSQL_DATA/
     mv /var/log/mysql/* $MYSQL_LOGS/
   fi
   rm -r /var/lib/mysql /var/log/mysql
   ln -s $MYSQL_DATA /var/lib/mysql
   ln -s $MYSQL_LOGS /var/log/mysql
-  chown -R mysql:mysql $MYSQL_DATA $MYSQL_LOGS /var/lib/mysql /var/log/mysql
+  chown -R mysql:mysql $MYSQL_CONF $MYSQL_DATA $MYSQL_LOGS /etc/mysql /var/lib/mysql /var/log/mysql
+
+  MYSQL_CONF_FILE="$MYSQL_CONF/mysql.conf.d/mysqld.cnf"
+  if [ ! -f $MYSQL_CONF_FILE ]; then
+    MYSQL_CONF_FILE="$MYSQL_CONF/my.cnf"
+  fi
+  sed -i 's/\(bind-address\)/#\1/' $MYSQL_CONF_FILE
+  sed -i '/\[mysqld\]/askip_name_resolve' $MYSQL_CONF_FILE
 fi
 
 if [ ! -f $MYSQL_INIT_SCRIPT ]; then
