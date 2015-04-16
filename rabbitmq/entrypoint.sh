2bd2f8552e7bb92d4980e89379288a3e6096f3d7
Vu Bui <vubui@qasymphony.com>
2015-04-16 12:36:50 +0700

    update rabbitmq to allow clustering setup

--- rabbitmq/entrypoint.sh
+++ rabbitmq/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 RABBITMQ_CONF='/conf'
+RABBITMQ_DATA='/data'
 
 if [ ! -f "$RABBITMQ_CONF/enabled_plugins" ]; then
   if  [ -z $RABBITMQ_PLUGINS ]; then
@@ -12,6 +13,21 @@ fi
 
 if [ ! -f "$RABBITMQ_CONF/rabbitmq.config" ]; then
   echo '[{rabbit, [{loopback_users, []}]}].' > $RABBITMQ_CONF/rabbitmq.config
+fi
+
+if [ ! -f "$HOME/.erlang.cookie" -a "$RABBITMQ_COOKIE" ]; then
+  echo $RABBITMQ_COOKIE > $HOME/.erlang.cookie
+  chmod 400 $HOME/.erlang.cookie
+fi
+
+if [ ! -d "$RABBITMQ_DATA/mnesia/rabbit@$(cat /etc/hostname)" -a "$RABBITMQ_CLUSTER_HOST" ]; then
+  if [ "$RABBITMQ_CLUSTER_IP" ]; then
+    echo "$RABBITMQ_CLUSTER_IP $RABBITMQ_CLUSTER_HOST" >> /etc/hosts
+  fi
+  rabbitmq-server -detached
+  rabbitmqctl stop_app
+  rabbitmqctl join_cluster rabbit@$RABBITMQ_CLUSTER_HOST
+  rabbitmqctl stop
 fi
 
 exec "$@"
